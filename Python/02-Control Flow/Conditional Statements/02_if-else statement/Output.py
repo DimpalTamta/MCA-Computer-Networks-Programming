4eb055666c6acbf6d7431759ae0a958ecad2fb13
DimByte <tamtadimpal654@gmail.com>
2025-10-04 18:18:38 +0530

    Create Output.py

---
+++ Python/02-Control Flow/Conditional Statements/02_if-else statement/Output.py
@@ -0,0 +1,10 @@
+#Sample Output when user enters age more than 18
+
+Enter your age: 20
+You are allowed to vote in the elections
+
+
+#Sample Output when user enters age less than 18
+
+Enter your age: 16
+You are not allowed to vote yet. Wait for a few more years!
