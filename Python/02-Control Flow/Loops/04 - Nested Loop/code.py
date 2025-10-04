1a13739df6064257f0e4f7447d30e6cc77c972aa
DimByte <tamtadimpal654@gmail.com>
2025-10-04 20:39:42 +0530

    Create code.py

---
+++ Python/02-Control Flow/Loops/04 - Nested Loop/code.py
@@ -0,0 +1,10 @@
+# -------------------------------
+# Nested Loops
+# -------------------------------
+
+# Definition: A loop inside another loop. Inner loop runs completely for each iteration of outer loop.
+
+print("========== NESTED LOOP Example ==========")
+for i in range(3):
+    for j in range(2):
+        print(f"i: {i} | j: {j}")
